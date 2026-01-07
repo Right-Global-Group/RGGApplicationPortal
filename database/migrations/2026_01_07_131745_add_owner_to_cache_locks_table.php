6ff913a67c1b42bf6a1628eedb6ff0ae85ccbe22
max-behrens-rgg <max.behrens@rightglobalgroup.com>
2026-01-07 13:18:11 +0000

    Scheduler fix.

---
+++ database/migrations/2026_01_07_131745_add_owner_to_cache_locks_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->string('owner')->after('key');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->dropColumn('owner');
+        });
+    }
+};
